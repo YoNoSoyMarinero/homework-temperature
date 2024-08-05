86f6d65e1827f482cbb002f2702aa34845ea904b
YoNoSoyMarinero <1milosmarinkovic1@gmail.com>
2024-08-06 00:41:54 +0200

    Set up pins, clear up the template

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-
-
 #![no_main]
 #![no_std]
 
 use cortex_m::{self};
 use cortex_m_rt::entry;
 use defmt_rtt as _;
-use panic_probe as _;
-use hal::{self, low_power, pac};
-
+use hal::gpio::{
+    Pin,
+    Port,
+    PinMode
+};
+use hal::pac::{Peripherals, SPI1};
+use hal::spi::{Spi, BaudRate};
 mod init;
 mod setup;
 mod system_status;
@@ -18,15 +20,18 @@ mod system_status;
 #[entry]
 fn main() -> ! {
 
-
     init::run();
-
+    let dp = Peripherals::take().unwrap();
+    //Setting up chip select and SPI communication
+    let mut cs = Pin::new(Port::A, 4, PinMode::Alt(5));
+    let mut spi: Spi<SPI1> = Spi::new(dp.SPI1,
+        Default::default(),
+        BaudRate::Div256);
     loop {
     }
 }
 
-// same panicking *behavior* as `panic-probe` but doesn't print a panic message
-// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
+
 #[defmt::panic_handler]
 fn panic() -> ! {
     cortex_m::asm::udf()
--- src/setup.rs
+++ src/setup.rs
@@ -1,18 +1,7 @@
-//! This module contains hardware configuration setup, eg GPIO, DMA, and IO.
-
-use hal::{
-    dma::{self, DmaInput, DmaInterrupt},
-    gpio::{OutputSpeed, Pin, PinMode, Port},
-};
+use hal::gpio::{Pin, PinMode, Port};
 
 pub fn setup_pins() {
-    // let mut can_rx = Pin::new(Port::A, 11, PinMode::Alt(9));
-    // let mut can_tx = Pin::new(Port::B, 9, PinMode::Alt(9));
-    //
-    // can_tx.output_speed(OutputSpeed::VeryHigh);
-    // can_rx.output_speed(OutputSpeed::VeryHigh);
-}
-
-pub fn setup_dma() {
-    // dma::enable_mux1();
+    let sck = Pin::new(Port::A, 5, PinMode::Alt(5)); 
+    let miso = Pin::new(Port::A, 6, PinMode::Alt(5));
+    let mosi: Pin = Pin::new(Port::A, 7, PinMode::Alt(5));
 }
